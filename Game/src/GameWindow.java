5d836d6879b7051844c76c39cac25ee49393f0d8
rvansoelen <vansoelen.1@osu.edu>
2013-11-02 15:41:05 -0400

    2nd

--- Game/src/GameWindow.java
+++ Game/src/GameWindow.java
@@ -1,10 +1,4 @@
-import javax.swing.JFrame;
 
 public class GameWindow {
-	public static void main(String[] args) {
-		JFrame frame = new JFrame("Mario Game");
-		frame.setSize(600, 600);
-		frame.setVisible(true);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	}
+
 }
